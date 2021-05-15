4ffd386a92b6915ff45c04fa8a87aab42b876c45
abdelrhman-basyoni <64443028+abdelrhman-basyoni@users.noreply.github.com>
2021-05-15 22:03:18 +0200

    added the jwt tokenauth

--- Backend/Task/Task/urls.py
+++ Backend/Task/Task/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+  
 urlpatterns = [
+    path('api/token/',
+         jwt_views.TokenObtainPairView.as_view(),
+         name ='token_obtain_pair'),
+    path('api/token/refresh/',
+         jwt_views.TokenRefreshView.as_view(),
+         name ='token_refresh'),
+    # path('', include('app.urls')),
+
     path('admin/', admin.site.urls),
 ]
